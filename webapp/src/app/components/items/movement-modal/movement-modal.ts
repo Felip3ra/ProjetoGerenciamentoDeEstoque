8bce782a655916553fc287304c96c005159637dc
Felipe Santana Santos <fsantos@iportsolutions.com.br>
2026-01-08 17:57:05 -0300

    feat: Criando Modal de Movimentos

---
+++ webapp/src/app/components/items/movement-modal/movement-modal.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule, X } from 'lucide-angular';
+
+@Component({
+  selector: 'app-movement-modal',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './movement-modal.html',
+  styleUrls: ['./movement-modal.css'],
+})
+export class MovementModal {
+  readonly CloseIcon = X;
+}
