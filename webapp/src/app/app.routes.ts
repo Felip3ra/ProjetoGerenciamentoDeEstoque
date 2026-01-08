92dc282dfc3032b5257a0f079b1784612fd9539a
Felipe Santana Santos <fsantos@iportsolutions.com.br>
2026-01-08 17:47:09 -0300

    feat: Criando main-layout

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { DashboardPage } from './pages/dashboard/dashboard';
+import { ItemsPage } from './pages/items/items';
+import { UsersPage } from './pages/users/users';
+import { MovementsPage } from './pages/movements/movements';
+import { ReportsPage } from './pages/reports/reports';
 
 export const routes: Routes = [
-    {
-        path: 'Login',
-        component: Login
-
-    }
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: 'dashboard', component: DashboardPage },
+      { path: 'items', component: ItemsPage },
+      { path: 'users', component: UsersPage },
+      { path: 'movements', component: MovementsPage },
+      { path: 'reports', component: ReportsPage },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+    ],
+  },
+  { path: '**', redirectTo: 'dashboard' },
 ];
