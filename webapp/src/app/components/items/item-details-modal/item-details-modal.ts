9a2b4d6a32403f5a282606df3cf23f76ef61b91a
Felipe Santana Santos <fsantos@iportsolutions.com.br>
2026-01-08 17:56:18 -0300

    feat: Criando Modal de detalhes

---
+++ webapp/src/app/components/items/item-details-modal/item-details-modal.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Calendar, FileText, Hash, History, LucideAngularModule, Package, User, X } from 'lucide-angular';
+
+@Component({
+  selector: 'app-item-details-modal',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './item-details-modal.html',
+  styleUrls: ['./item-details-modal.css'],
+})
+export class ItemDetailsModal {
+  readonly CloseIcon = X;
+  readonly PackageIcon = Package;
+  readonly HashIcon = Hash;
+  readonly FileTextIcon = FileText;
+  readonly CalendarIcon = Calendar;
+  readonly UserIcon = User;
+  readonly HistoryIcon = History;
+}
