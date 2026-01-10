b67f6e821cc45b477cc369de49ad09e348228bc7
Felipe <felipesantanasantos1812@gmail.com>
2026-01-09 23:20:47 -0300

    fix: Ajuste na regra de acesso

--- webapp/src/app/components/users/user-create-modal/user-create-modal.ts
+++ webapp/src/app/components/users/user-create-modal/user-create-modal.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-user-create-modal',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './user-create-modal.html',
   styleUrls: ['./user-create-modal.css'],
 })
 export class UserCreateModal {
   readonly CloseIcon = X;
   @Output() close = new EventEmitter<void>();
+  hasAccess = true;
+
+  setAccess(event: Event) {
+    const target = event.target as HTMLSelectElement | null;
+    this.hasAccess = target?.value === 'com';
+  }
 }
--- webapp/src/app/components/users/user-edit-modal/user-edit-modal.ts
+++ webapp/src/app/components/users/user-edit-modal/user-edit-modal.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LucideAngularModule, X } from 'lucide-angular';
 
 @Component({
   selector: 'app-user-edit-modal',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './user-edit-modal.html',
   styleUrls: ['./user-edit-modal.css'],
 })
 export class UserEditModal {
   readonly CloseIcon = X;
   @Output() close = new EventEmitter<void>();
+  hasAccess = true;
+
+  setAccess(event: Event) {
+    const target = event.target as HTMLSelectElement | null;
+    this.hasAccess = target?.value === 'com';
+  }
 }
